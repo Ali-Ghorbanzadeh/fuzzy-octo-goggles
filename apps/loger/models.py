e858fc9e46a73c550791baa03cae075183db1b62
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-11-07 14:48:17 +0330

    base

---
+++ apps/loger/models.py
@@ -0,0 +1,24 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Log(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action_type = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+"""
+Model Profile
+1 - ALi
+2 - Reza
+
+Model Log
+1 - Profile 2
+
+"""
